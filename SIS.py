b4dddf649241b3f131ac7704debd45e51fda4471
YiRanZhang <479931576@qq.com>
2021-11-28 21:30:25 +0800

    SIS Model packaged

--- SIS.py
+++ SIS.py
@@ -4,114 +4,89 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-Node_num = 1000
-Coefficient = 1
-Link_p = Coefficient*np.log(Node_num)/Node_num
+"""
+Function
+----------
+This function aims to achieve the simulation of SIS Model
 
-BeginT = time.time()
+Parameters
+----------
+1. G : Input Graph
+2. Infected : The ndarray including the nodes infected at first
+3. Gamma: Transmission probability
+4. Beta: Recovery probability
+5. Epoch : The iterations in total
+"""
+def Evolution_SIS(G, Infected, Gamma = 0.5, Beta = 0.50, Epoch = 20):
 
-# Generate the random infected node
-Infected_init_num = 10
-Infected = []
-while(len(Infected) < Infected_init_num):
-    x = random.randint(0, Node_num - 1)
-    if x not in Infected:
-        Infected.append(x)
-print("1.Infected nodes are generated!")
+    # Get the number of the nodes
+    Node_num = len(G.nodes)
 
-GenerateNodeT = time.time()
+    # Initialize the state of the nodes, 1 denotes a healthy node while 0 denotes an infected node
+    Node_state = np.ones(Node_num).astype(np.int8)
+    Node_state[Infected] = 0
 
-# Initialize the state of the nodes, 1 denotes a healthy node while 0 denotes an infected node
-Node_state = np.ones(Node_num).astype(np.int8)
-Node_state[Infected] = 0
+    # Get the Adjacent matrix of the Graph
+    A = np.array(nx.adjacency_matrix(G).astype(np.int8).todense())
 
-# Create the Random Graph
-er = nx.erdos_renyi_graph(Node_num, Link_p)
-ps = nx.shell_layout(er)
+    print("3.Adjacent Matrix is prepared!")
 
-while nx.is_connected(er) == False:
-    print("Fail to create!")
-    er = nx.erdos_renyi_graph(Node_num, Link_p)
-    ps = nx.shell_layout(er)
+    x_label = []
+    Infected_label = []
+    Recovery_label = []
 
-GenerateNetworkT = time.time()
+    ### Problem describe
+    for i in range(1, Epoch):
 
-print("2.Network is generated!")
+        x_label.append(i)
+        # Fisrt Control the recovery of the Node Infected
 
-# Get the Adjacent matrix of the Graph
-A = np.array(nx.adjacency_matrix(er).astype(np.int8).todense())
+        # Deep copy of Node_state
+        Tmp_Node_state = Node_state.copy()
+        zero_pos = np.array(np.where(Tmp_Node_state == 0)[0])
+        one_pos = np.array(np.where(Tmp_Node_state == 1)[0])
 
-print("3.Adjacent Matrix is prepared!")
+        Tmp_Node_state[zero_pos] = 1
+        Tmp_Node_state[one_pos] = 0
 
-# Define the transmission probability
-Gamma = 0.50      # Transmission probability
-Beta = 0.50       # Recovery probability
+        x = np.random.rand(Node_num)
+        x = x * Tmp_Node_state
+        Recovry_iter = np.array(np.intersect1d(np.where(x < Beta)[0], np.where(x > 0)[0]))
 
-Last_Infected = Infected_init_num
+        #print("Recovery in this iter is : "+ str(len(Recovry_iter)))
+        Recovery_label.append(len(Recovry_iter))
 
-x_label = []
-Infected_label = []
-Recovery_label = []
+        Node_state[Recovry_iter] = 1
 
-### Problem describe
-for i in range(1, 120):
+        Infected = np.setdiff1d(Infected, Recovry_iter)
 
-    x_label.append(i)
-    # Fisrt Control the recovery of the Node Infected
+        Tmp = Infected.copy()
 
-    # Deep copy of Node_state
-    Tmp_Node_state = Node_state.copy()
-    zero_pos = np.array(np.where(Tmp_Node_state == 0)[0])
-    one_pos = np.array(np.where(Tmp_Node_state == 1)[0])
+        # Second Control the Node Infected
+        for infected_index in Infected:
+            # Generate the array containing all the random seed's state
+            y = np.random.rand(Node_num)
+            y = y * Node_state
+            y = y * A[infected_index]
 
-    Tmp_Node_state[zero_pos] = 1
-    Tmp_Node_state[one_pos] = 0
+            # Get the state of this iter
+            Infected_iter = np.array(np.intersect1d(np.where(y < Gamma)[0],np.where(y > 0)[0]))
 
-    x = np.random.rand(Node_num)
-    x = x * Tmp_Node_state
-    Recovry_iter = np.array(np.intersect1d(np.where(x < Beta)[0], np.where(x > 0)[0]))
+            # Refresh the spreading in real time
+            Node_state[Infected_iter] = 0
 
-    #print("Recovery in this iter is : "+ str(len(Recovry_iter)))
-    Recovery_label.append(len(Recovry_iter))
+            # Refresh the state of the nodes
+            Tmp = np.union1d(Infected_iter, Tmp)
 
-    Node_state[Recovry_iter] = 1
+        Infected = Tmp.copy()
+        Infected_label.append(len(Infected))
 
-    Infected = np.setdiff1d(Infected, Recovry_iter)
+    plt.xlabel("Iteration")
+    plt.ylabel("Number")
+    plt.plot(x_label,Recovery_label,label="Recovered")
+    plt.plot(x_label,Infected_label,label="Infected")
+    plt.legend()
+    plt.savefig("Output/"+str(G.name)+"/output_SIS.jpg")
+    plt.close()
 
-    Tmp = Infected.copy()
-
-    # Second Control the Node Infected
-    for infected_index in Infected:
-        # Generate the array containing all the random seed's state
-        y = np.random.rand(Node_num)
-        y = y * Node_state
-        y = y * A[infected_index]
-
-        # Get the state of this iter
-        Infected_iter = np.array(np.intersect1d(np.where(y < Gamma)[0],np.where(y > 0)[0]))
-
-        # Refresh the spreading in real time
-        Node_state[Infected_iter] = 0
-
-        # Refresh the state of the nodes
-        Tmp = np.union1d(Infected_iter, Tmp)
-
-    Infected = Tmp.copy()
-    Infected_label.append(len(Infected))
-
-FinishT = time.time()
-
-print("Init the nodes cost : "+str(GenerateNodeT - BeginT))
-print("Gegerate Network cost : "+str(GenerateNetworkT - GenerateNodeT))
-print("Calculation cost : "+str(FinishT - GenerateNetworkT))
-
-plt.xlabel("Iteration")
-plt.ylabel("Number")
-plt.plot(x_label,Recovery_label,label="Infected")
-plt.plot(x_label,Infected_label,label="Recovered")
-plt.legend()
-plt.savefig("output.jpg")
-
-# nx.draw(er, ps, with_labels = True, node_size = Node_num, font_size = 8, node_color = "#CCFFFF")
-# nx.draw(er, ps, with_labels = True, node_size = Node_num, font_size = 8, node_color = "#ff3333", nodelist = Infected)
-# plt.show()
+    return Infected_label, Recovery_label
