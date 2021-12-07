f530157247ddea78c72ccb9b2a1af77bf323a89c
YiRanZhang <479931576@qq.com>
2021-12-07 17:28:15 +0800

    Adding the realization of Karate-club network

--- Network.py
+++ Network.py
@@ -5,32 +5,20 @@ import time
 import matplotlib.pyplot as plt
 from Tools import *
 
-"""
-Function
-----------
-This function aims to get an ER Network
+def ER_Network(Node_num = 1000, K = 20, Connectivity = True):
+    """
+    Function
+    ----------
+    This function aims to get an ER Network
 
-Parameters
-----------
-1. Node_num : The number of the network
-2. K: The average degree of the network
-3. Infected_init_num: The number of the vertices infected at first
-4. Connectivity: Determine whether the ER network generated should be fully connected
-"""
-
-def ER_Network(Node_num = 1000, K = 20, Infected_init_num = 10, Connectivity = True):
-
+    Parameters
+    ----------
+    1. Node_num : The number of the network
+    2. K: The average degree of the network
+    3. Connectivity: Determine whether the ER network generated should be fully connected
+    """
     # Calculate the Link_p
     Link_p = K / Node_num
-
-    # Generate the random infected node
-    Infected = []
-    while (len(Infected) < Infected_init_num):
-        x = random.randint(0, Node_num - 1)
-        if x not in Infected:
-            Infected.append(x)
-
-    print("1.Infected nodes are generated!")
 
     # Create the ER Graph
     G = nx.erdos_renyi_graph(Node_num, Link_p)
@@ -39,76 +27,60 @@ def ER_Network(Node_num = 1000, K = 20, Infected_init_num = 10, Connectivity = T
     # Ensure the network is fully connected and the error of <k> is tolerable
     if Connectivity == True:
         while nx.is_connected(G) == False or Degree_bias(G, K) > 0.001:
+            #print(Degree(G))
             #print("Fail to create!")
             G = nx.erdos_renyi_graph(Node_num, Link_p)
     else:
         while Degree_bias(G, K) > 0.001:
             G = nx.erdos_renyi_graph(Node_num, Link_p)
 
-    print("2.Network is generated!")
+    # print("1.ER Network is generated!")
 
     G.name = "ER"
 
-    return G, Infected
+    return G
 
-"""
-Function
-----------
-This function aims to get an WS Network
 
-Parameters
-----------
-1. Node_num : The number of the network
-2. K: The average degree of the network
-3. Link_p: The reconnect probability
-4. Infected_init_num: The number of the vertices infected at first
-"""
-def WS_Network(Node_num = 1000, K = 20, Link_p = 0.5, Infected_init_num = 10):
+def WS_Network(Node_num = 1000, K = 20, Link_p = 0.5):
+    """
+    Function
+    ----------
+    This function aims to get an WS Network
 
-    # Generate the random infected node
-    Infected = []
-    while (len(Infected) < Infected_init_num):
-        x = random.randint(0, Node_num - 1)
-        if x not in Infected:
-            Infected.append(x)
-
-    print("1.Infected nodes are generated!")
+    Parameters
+    ----------
+    1. Node_num : The number of the network
+    2. K: The average degree of the network
+    3. Link_p: The reconnect probability
+    """
 
     # Create the ER Graph
     G = nx.random_graphs.watts_strogatz_graph(Node_num, K, Link_p)
 
     # Ensure the network is fully connected and the error of <k> is tolerable
     while nx.is_connected(G) == False or Degree_bias(G, K) > 0.001:
+        #print(Degree(G))
         #print("Fail to create!")
         G = nx.random_graphs.watts_strogatz_graph(Node_num, K, Link_p)
 
-    print("2.Network is generated!")
+    print("1.WS Network is generated!")
 
     G.name = "WS"
 
-    return G, Infected
+    return G
 
-"""
-Function
-----------
-This function aims to get an BA Network
 
-Parameters
-----------
-1. Node_num : The number of the network
-2. Edge_num: The edge added per round, nearly equals the half of the <k>
-3. Infected_init_num: The number of the vertices infected at first
-"""
-def BA_Network(Node_num = 1000, Edge_num = 1, Infected_init_num = 10):
+def BA_Network(Node_num = 1000, Edge_num = 1):
+    """
+    Function
+    ----------
+    This function aims to get an BA Network
 
-    # Generate the random infected node
-    Infected = []
-    while (len(Infected) < Infected_init_num):
-        x = random.randint(0, Node_num - 1)
-        if x not in Infected:
-            Infected.append(x)
-
-    print("1.Infected nodes are generated!")
+    Parameters
+    ----------
+    1. Node_num : The number of the network
+    2. Edge_num: The edge added per round, nearly equals the half of the <k>
+    """
 
     # Create the ER Graph
     G = nx.random_graphs.barabasi_albert_graph(Node_num, Edge_num)
@@ -118,27 +90,23 @@ def BA_Network(Node_num = 1000, Edge_num = 1, Infected_init_num = 10):
         G = nx.random_graphs.barabasi_albert_graph(Node_num, Edge_num)
         ps = nx.shell_layout(G)
 
-    print("2.Network is generated!")
+    print("1.BA Network is generated!")
 
     G.name = "BA"
 
-    return G, Infected
+    return G
 
+def Real_Network(File_path):
+    """
+    Function
+    ----------
+    This function aims to get an Network from the real dataset
 
-"""
-Function
-----------
-This function aims to get an Network from the real dataset
- 
-Parameters
-----------
-1. File_path : The Path of the dataset
-2. Infected_init_num: The number of the vertices infected at first
-"""
-def Real_Network(File_path, Infected_init_num = 10):
-
+    Parameters
+    ----------
+    1. File_path : The Path of the dataset
+    """
     # Create the ScaleFree Graph
-    ID = []
 
     G = nx.Graph()
     for line in open(File_path):
@@ -146,21 +114,24 @@ def Real_Network(File_path, Infected_init_num = 10):
         n1 = int(strlist[0])
         n2 = int(strlist[1])
         G.add_edges_from([(n1, n2)])  # G.add_edges_from([(n1, n2)])
-        ID.append(n1)
-        ID.append(n2)
 
-    ID = np.unique(ID)
-    # Generate the random infected node
-    Infected = []
-    while (len(Infected) < Infected_init_num):
-        x = random.randint(0, len(ID) - 1)
-        if ID[x] not in Infected:
-            Infected.append(ID[x])
-
-    print("1.Infected nodes are generated!")
-
-    print("2.Network is generated!")
+    print("1.Real Network Network is generated!")
 
     G.name = "ScaleFree"
 
-    return G, Infected
+    return G
+
+
+
+def Karate_club_Network():
+    """
+    Function
+    ----------
+    This function aims to get a Karate-club Network
+
+    """
+    G = nx.karate_club_graph()
+
+    G.name = "Karate"
+
+    return G
